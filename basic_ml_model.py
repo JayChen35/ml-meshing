49e56152c46da85dcf11ade69fea9197647b805b
JayChen35 <jay.chen135@gmail.com>
2024-11-01 23:09:24 -0400

    Add working training loop with updated S loss function, but not yet converging on loss

--- basic_ml_model.py
+++ basic_ml_model.py
@@ -3,7 +3,7 @@
 
 import torch
 from torch import nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 import numpy as np
 from scipy.linalg import logm
 import pandas as pd
@@ -14,6 +14,7 @@ from readgri import readgri
 LEARNING_RATE = 1e-3
 BATCH_SIZE = 32
 EPOCHS = 5
+TRAIN_FRACTION = 0.8
 
 
 class GigaMesher9000(nn.Module):
@@ -126,8 +127,9 @@ class MetricLoss(nn.Module):
             M = torch.stack([torch.stack([act[0], act[1]]), torch.stack([act[1], act[2]])])
             M_0_temp = torch.linalg.inv(matrix_operator(M_0, 1/2))
             S = matrix_operator(M_0_temp @ M @ M_0_temp, "log")
-            total_loss += torch.sum(torch.diagonal(S))
-        return abs(total_loss / len(prediction))
+            # Take Frobenius norm of S, ignoring the square root at the end
+            total_loss += torch.sum(torch.square(S))
+        return total_loss / len(prediction)
 
 
 def matrix_operator(A: torch.Tensor, operator) -> torch.Tensor:
@@ -149,11 +151,11 @@ def matrix_operator(A: torch.Tensor, operator) -> torch.Tensor:
 def train_loop(data_loader: DataLoader, model: nn.Module, loss_fn, optimizer, device):
     # Set the model to training mode
     model.train()
-    for batch_i, sample_batched in enumerate(data_loader):
-        features, labels = sample_batched
+    for batch_i, (features, labels) in enumerate(data_loader):
+        features, labels = features.to(device), labels.to(device)
         # Labels is of shape [BATCH_SIZE, 3] since (A,B,C) of metric field
-        pred = model(features.to(device))  # Passing an entire batch size into the model
-        loss = loss_fn(pred, labels.to(device))
+        pred = model(features)  # Passing an entire batch size into the model
+        loss = loss_fn(pred, labels)
         # Backpropagation
         loss.backward()
         optimizer.step()
@@ -163,6 +165,23 @@ def train_loop(data_loader: DataLoader, model: nn.Module, loss_fn, optimizer, de
         if batch_i % 50 == 0:
             current, total_data = batch_i * BATCH_SIZE, len(data_loader) * BATCH_SIZE
             print(f"Loss: {loss.item():>7f}  [{current:>5d}/{total_data:>5d}]")
+
+
+def test_loop(data_loader: DataLoader, model: nn.Module, loss_fn, device):
+    # Set the model to evaluation mode
+    model.eval()
+    num_batches = len(data_loader)
+    test_loss = 0
+    # torch.no_grad() ensures that no gradients are computed during test mode
+    # Also reduces unnecessary computations for tensors with requires_grad=True
+    with torch.no_grad():
+        for feature, label in data_loader:
+            feature, label = feature.to(device), label.to(device)
+            pred = model(feature)
+            test_loss += loss_fn(pred, label).item()
+
+    test_loss /= num_batches
+    print(f"Testing average loss: {test_loss:>8f} \n")
 
 
 if __name__ == "__main__":
@@ -178,16 +197,21 @@ if __name__ == "__main__":
     # Format data from .gri files to feed into training and testing
     folder_path = "./meshes/"
     gri_regex = "run.*gri"
-    data_set = MeshDataset(folder_path, gri_regex, transform=ToTensor())
-    data_loader = DataLoader(data_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+    print("Initializing data set...")
+    dataset = MeshDataset(folder_path, gri_regex, transform=ToTensor())
+    train_size = int(TRAIN_FRACTION * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
     model = GigaMesher9000().to(device)
     loss_fn = MetricLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
     
     for t in range(EPOCHS):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train_loop(data_loader, model, loss_fn, optimizer, device)
-        # test(test_dataloader, model, loss_fn)
+        print(f"----------------Epoch {t+1}---------------")
+        train_loop(train_loader, model, loss_fn, optimizer, device)
+        test_loop(test_loader, model, loss_fn, device)
     print("Done!")
     
